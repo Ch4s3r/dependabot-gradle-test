351d9b6ef6e9366e370962d5cb99e44f494febcf
Ch4s3r <lamprecht.patrick1@gmail.com>
2023-10-02 22:16:29 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/dependabotgradletest/DependabotGradleTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dependabotgradletest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DependabotGradleTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
